60738956a318124f49e68230243b95720a548bae
Alessandro <81766448+SkyAle-bit@users.noreply.github.com>
2023-11-15 15:31:53 +0100

    Update UserManger.java

--- UserManger.java
+++ UserManger.java
@@ -4,95 +4,62 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class UserDataManager {
-    private static final Map<String, String> userData = new HashMap<>();
-    private static final String USER_DATA_FILE_PATH = "user_data_backup.txt";
+public class PasswordManager {
+    private static final Map<String, String> passwordData = new HashMap<>();
+    private static final String PASSWORD_DATA_FILE_PATH = "password_data_backup.txt";
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        createData(scanner);
-        readData();
-        updateData(scanner);
-        deleteData();
+        changePassword(scanner);
 
         scanner.close();
     }
 
-    public static void createData(Scanner scanner) {
-        System.out.println("Parte 3: CRUD di Dati Utente - Create");
+    public static void changePassword(Scanner scanner) {
+        System.out.println("Parte 4: Funzione Aggiuntiva - Cambio Password");
         if (isLoggedIn()) {
-            System.out.print("Inserisci nuove informazioni: ");
-            String newData = scanner.nextLine();
+            System.out.print("Inserisci la vecchia password: ");
+            String oldPassword = scanner.nextLine();
 
-            if (validateData(newData)) {
-                userData.put(AccountManager.getLoggedInUser(), newData);
-                System.out.println("Informazioni aggiunte con successo.");
+            if (verifyOldPassword(oldPassword)) {
+                String newPassword = requestNewPassword(scanner);
+
+                updatePassword(newPassword);
+
+                System.out.println("Password cambiata con successo.");
                 backupData();
             } else {
-                System.out.println("Errore: Le informazioni non sono valide.");
+                System.out.println("Errore: Vecchia password non corretta.");
             }
         } else {
-            System.out.println("Devi effettuare l'accesso per aggiungere informazioni.");
+            System.out.println("Devi effettuare l'accesso per cambiare la password.");
         }
     }
 
-    public static void readData() {
-        System.out.println("Parte 3: CRUD di Dati Utente - Read");
-        if (isLoggedIn()) {
-            String data = userData.get(AccountManager.getLoggedInUser());
-            if (data != null) {
-                System.out.println("Le tue informazioni: " + data);
-            } else {
-                System.out.println("Nessuna informazione disponibile.");
-            }
-        } else {
-            System.out.println("Devi effettuare l'accesso per visualizzare le informazioni.");
-        }
+    private static boolean verifyOldPassword(String oldPassword) {
+        return passwordData.containsKey(AccountManager.getLoggedInUser()) &&
+                passwordData.get(AccountManager.getLoggedInUser()).equals(AccountManager.encryptPassword(oldPassword));
     }
 
-    public static void updateData(Scanner scanner) {
-        System.out.println("Parte 3: CRUD di Dati Utente - Update");
-        if (isLoggedIn()) {
-            System.out.print("Inserisci nuove informazioni: ");
-            String newData = scanner.nextLine();
-
-            if (validateData(newData)) {
-                userData.put(AccountManager.getLoggedInUser(), newData);
-                System.out.println("Informazioni aggiornate con successo.");
-                backupData();
-            } else {
-                System.out.println("Errore: Le informazioni non sono valide.");
-            }
-        } else {
-            System.out.println("Devi effettuare l'accesso per aggiornare le informazioni.");
-        }
+    private static String requestNewPassword(Scanner scanner) {
+        System.out.print("Inserisci la nuova password: ");
+        return scanner.nextLine();
     }
 
-    public static void deleteData() {
-        System.out.println("Parte 3: CRUD di Dati Utente - Delete");
-        if (isLoggedIn()) {
-            userData.remove(AccountManager.getLoggedInUser());
-            System.out.println("Informazioni eliminate con successo.");
-            backupData();
-        } else {
-            System.out.println("Devi effettuare l'accesso per eliminare le informazioni.");
-        }
-    }
-
-    private static boolean validateData(String data) {
-        return data.length() >= 5;
+    private static void updatePassword(String newPassword) {
+        passwordData.put(AccountManager.getLoggedInUser(), AccountManager.encryptPassword(newPassword));
     }
 
     private static void backupData() {
-        try (FileWriter writer = new FileWriter(USER_DATA_FILE_PATH)) {
-            for (Map.Entry<String, String> entry : userData.entrySet()) {
+        try (FileWriter writer = new FileWriter(PASSWORD_DATA_FILE_PATH)) {
+            for (Map.Entry<String, String> entry : passwordData.entrySet()) {
                 writer.write(entry.getKey() + ":" + entry.getValue() + "\n");
             }
-            System.out.println("Backup dei dati completato con successo.");
+            System.out.println("Backup dei dati delle password completato con successo.");
         } catch (IOException e) {
-            System.out.println("Errore durante il backup dei dati: " + e.getMessage());
+            System.out.println("Errore durante il backup dei dati delle password: " + e.getMessage());
         }
     }
 
